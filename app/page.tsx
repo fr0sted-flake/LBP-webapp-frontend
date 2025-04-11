001b9577087c22c71c5f1633ca85f410e7dfff66
Ayush Bhardwaj <fr0sted.flake.02@gmail.com>
2025-04-11 15:40:38 +0530

    added url

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,8 @@ export default function Home() {
     setError(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      // http://127.0.0.1:8000/predict
+      const response = await fetch("https://lbp-webapp-backend.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
